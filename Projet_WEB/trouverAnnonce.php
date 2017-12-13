612c1cdf4cc8c71038fcb8a5168def9265baab53
Anonyclone <Anonyclone@users.noreply.github.com>
2017-12-13 17:01:38 +0100

    Add files via upload

---
+++ Projet_WEB/trouverAnnonce.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Trouver une annonce</title>
+</head>
+<body>
+	<header><h1>Trouver une annonce </h1></header>
+
+<form action="" method="post">
+
+	<p>Ville de départ :
+		<input type="text" name="vDep" />
+	</p>
+	<p>Ville d'arrivée : 
+		<input type="text" name="vArr" />
+	</p>	
+	<p>Date : 
+		<input type="date" name="date" />
+	</p>
+
+<br/><br/>
+		<input type="submit" value="Chercher un trajet"/> <input type="submit" value="Retour">
+</form>
+</body>
+</html>
