86b5107f31a25b965b5221e58862148e0335cb3f
maximilienlix <maximiliendidio@protonmail.com>
2017-12-13 17:32:03 +0100

    integration de l'api google map

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,5 +19,19 @@
             </div>
         </div>
     </div>
+    <h1>Calcul d'itinéraire Google Maps Api V3</h1>
+    <div id="destinationForm">
+        <form action="" method="get" name="direction" id="direction">
+            <label>Point de départ :</label>
+            <input type="text" name="origin" id="origin">
+            <label>Destination :</label>
+            <input type="text" name="destination" id="destination">
+            <input type="button" value="Calculer l'itinéraire" onclick="javascript:calculate()">
+        </form>
+    </div>
+    <div id="panel"></div>
+    <div id="map">
+        <p>Veuillez patienter pendant le chargement de la carte...</p>
+    </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,14 +9,14 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Portail Tartempion</title>
-
+    <link rel="stylesheet" href="{{ URL::asset('css/map/jquery-ui-1.8.12.custom.css') }}" type="text/css" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::asset('css/custom.css') }}">
     <!--
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{ URL::asset('css/custom.css') }}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 </head>
 <body>
@@ -82,5 +82,10 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+
+    <script type="text/javascript" src="{{ URL::asset('js/map/jquery.min.js')}}"></script>
+    <script type="text/javascript" src="{{ URL::asset('js/map/jquery-ui-1.8.12.custom.min.js')}}"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&language=fr"></script>
+    <script type="text/javascript" src="{{ URL::asset('js/map/functions.js')}}"></script>
 </body>
 </html>
