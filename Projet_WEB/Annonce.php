e2cf17be2e5543f94b9dcbbf9adc59f36f9e7d91
BenhoIT <hobenoit@gmail.com>
2017-12-15 10:23:26 +0100

    Add files via upload
    Meilleure presentation du code + ajout bootstrap

--- Projet_WEB/Annonce.php
+++ Projet_WEB/Annonce.php
@@ -3,32 +3,59 @@
     <head>
         <title>Formulaire pour mettre en ligne une annonce</title>
         <meta charset="utf-8" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     </head>
     <body>
         <h1>Mise en ligne d'annonce</h1>
-        <form action="cible.php" method="post">
-        <p>
-            <B>Remplissez le formulaire suivant afin de mettre en ligne votre annonce de trajet</B><br/>
-
-
-            <blockquote>Lieu de départ : <blockquote><input type="text" name="depart"><br/></blockquote></blockquote>
-            <blockquote>Lieu de d'arrivée : <blockquote><input type="text" name="arrivee"><br/></blockquote></blockquote>
-            <blockquote>Date du covoiturage : <blockquote><input type="date" name="dateCov"><br/></blockquote></blockquote>
-            <blockquote>Heure du covoiturage : <blockquote><input type="time" name="heureCov"><br/></blockquote></blockquote>
-            <blockquote>Nombre de place : <blockquote><input type="number" name="place"><br/></blockquote></blockquote> 
-            <blockquote>Nombre de place pour baggage(s) : <blockquote><input type="number" name="baggage"><br/></blockquote></blockquote> 
-            <blockquote>Durée : (calculée automatiquement avec)<blockquote><input type="time" name="duree"><br/></blockquote></blockquote>
-            <blockquote>Type de véhicule :  
-              <select type="text" name="vehicule"><br/>
-                  <option value="citadine">Citadine</option>
-                  <option value="suv">SUV</option>
-                  <option value="break">Break</option>
-                  <option value="bus">Bus</option>
-              </select></blockquote></blockquote>
-            <blockquote><p>Acceptez-vous les fumeurs ?</p>
-            <blockquote><p><label><input type="radio" name="fumeur" value="oui"/>Oui</label>
-            <label><input type="radio" name="fumeur" value="non"/>Non</label></p></blockquote></blockquote>
-            <blockquote><input type="submit" value="Valider"></blockquote>
-        </p>
+        <form>
+            <div class="form-group-fluid">
+              <label for="LieuDepart">Lieu de départ : </label>
+              <input type="text" name="depart" placeholder="Entrez le lieu de départ">
+            </div>
+            <div class="form-group-fluid">
+               <label for="LieuArrivee">Lieu de d'arrivée : </label>
+               <input type="text" name="arrivee" placeholder="Entrez le lieu d'arrivée">
+            </div>
+            <div class="form-group-fluid">         
+                <label for="DateCovoit">Date du covoiturage : </label>
+                <input type="date" name="dateCov">
+            </div>
+            <div class="form-group-fluid">
+               <label for="HeureCovoit">Heure du covoiturage : </label>
+               <input type="time" name="heureCov">
+            </div>
+            <div class="form-group-fluid">
+              <label for="nbPlace">Nombre de place : </label>
+              <input type="number" name="place">
+              <small id="placeHelp" class="form-text text-muted">Entrez le nombre de place(s) dans votre véhicule.</small>
+            </div>
+            <div class="form-group-fluid">
+              <label for="nbBaggage">Nombre de place pour baggage(s) : </label>
+              <input type="number" name="baggage">
+              <small id="baggageHelp" class="form-text text-muted">Entrez le nombre de baggage(s) qui peuvent rentrer dans votre véhicule.</small>
+            </div>
+            <div class="form-group-fluid">
+              <label for="Duree">Durée : (calculée automatiquement avec)</label>
+              <input type="time" name="duree">
+            </div>
+            <div class="form-group-fluid">
+              <label for="TypeVehicule">Type de véhicule : </label>
+                <select type="text" name="vehicule"><br/>
+                    <option value="citadine">Citadine</option>
+                    <option value="suv">SUV</option>
+                    <option value="break">Break</option>
+                    <option value="bus">Bus</option>
+                </select>
+            </div>
+            <div class="form-group-fluid">
+                <label for="Fumeur">Acceptez-vous les fumeurs ?</label>
+                <label><input type="radio" name="fumeur" value="oui"/>Oui</label>
+                <label><input type="radio" name="fumeur" value="non"/>Non</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Valider</button>
+        </form>
     </body>
 </html>
