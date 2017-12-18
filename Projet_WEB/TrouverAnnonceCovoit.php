ec56c8eab59d66d704c214aff30022e1a7a99f75
BenhoIT <hobenoit@gmail.com>
2017-12-18 10:44:15 +0100

    Update TrouverAnnonceCovoit.php
    Correction titre

--- Projet_WEB/TrouverAnnonceCovoit.php
+++ Projet_WEB/TrouverAnnonceCovoit.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Formulaire pour mettre en ligne une annonce de covoiturage</title>
+        <title>Formulaire pour trouver en ligne une annonce de covoiturage</title>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
